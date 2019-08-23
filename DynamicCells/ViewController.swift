6f2d60bf0e856a81b3ccb5882eca7ab351339abb
Filip Zymek <zymfilip@amazon.com>
2019-08-23 09:41:54 +0100

    Initial Commit

---
+++ DynamicCells/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicCells
+//
+//  Created by Zymek, Filip on 23/08/2019.
+//  Copyright © 2019 Zymek, Filip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
